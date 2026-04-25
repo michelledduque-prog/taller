71a7504a28377fa0cf586cbe6ac1f77adbe9a7ee
dayanna duque <michelledayanna0@gmail.com>
2026-04-24 19:14:07 -0500

    complementando entidad docente

--- src/main/java/com/uniajc/vista/VistaDocente.java
+++ src/main/java/com/uniajc/vista/VistaDocente.java
@@ -1,3 +1,31 @@
+package com.uniajc.vista;
 
+import com.uniajc.controlador.ControladorDocente;
+import java.util.Scanner;
+
+public class VistaDocente {
+
+    public void crearDocente() {
+
+        Scanner sc = new Scanner(System.in);
+        ControladorDocente c = new ControladorDocente();
+
+        System.out.println("=== DOCENTE ===");
+
+        System.out.print("Nombre: ");
+        String nombre = sc.nextLine();
+
+        System.out.print("Apellido: ");
+        String apellido = sc.nextLine();
+
+        System.out.print("Email: ");
+        String email = sc.nextLine();
+
+        
+        c.crear(nombre, apellido);
+
+        System.out.println("Docente guardado correctamente");
+    }
+}
 
 
